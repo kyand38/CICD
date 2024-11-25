14b5104743ecc8ed470302c2110c2cd838960592
kyand38 <kyand2024@gmail.com>
2024-11-25 11:43:03 -0600

    circlci

--- Develop/cypress.config.ts
+++ Develop/cypress.config.ts
@@ -1,20 +1,27 @@
-import { defineConfig } from 'cypress';
-import viteConfig from './vite.config';
+import { defineConfig } from "cypress";
+import coverageTask from "@cypress/code-coverage/task";
 
 export default defineConfig({
-  component: {
-    port: 5173,
-    devServer: {
-      framework: 'react',
-      bundler: 'vite',
-      viteConfig,
-    },
-  },
+	component: {
+		devServer: {
+			framework: "react",
+			bundler: "vite"
+		},
+		specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
+		setupNodeEvents(on, config) {
+			coverageTask(on, config);
 
-  e2e: {
-    baseUrl: 'http://localhost:3001',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+			return config;
+		},
+	},
+
+	e2e: {
+		baseUrl: "http://localhost:3000",
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+			coverageTask(on, config);
+
+			return config;
+		},
+	},
 });
